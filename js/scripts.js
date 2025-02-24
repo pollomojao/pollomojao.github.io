039b0f07e5b27bee50415ca30ad8facf0a37d0d9
pollomojao <misospiceynpollomojao@gmail.com>
2025-02-24 02:30:55 -0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Función para mostrar sorpresa
+document.getElementById('surpriseBtn').addEventListener('click', function() {
+    // Reproducir la canción
+    document.getElementById('audio').play();
+
+    // Mostrar el gif
+    document.getElementById('gifContainer').style.display = 'block';
+
+    // Cambiar el mensaje
+    document.getElementById('message').innerHTML = '🌟 Cuídate mucho, te dejo un miso spicy para ti. <span id="ramenEmoji">🍜</span> 🌟';
+
+    //Cambiar h1
+    document.getElementById('title').innerHTML = 'Elfo, lo siento mucho'
+    // Cambiar el texto del botón
+    this.textContent = '¡Canción otaku!';
+
+    //Mostrar el footer
+    document.querySelector('.footer').style.display = 'block';
+
+});
